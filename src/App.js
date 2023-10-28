72afc90f7acec5c0ddcdc9969796d559c5e3be3d
Adrian Wozniak <adrianwozniak20@gmail.com>
2023-10-28 20:09:16 +0200

    added Form component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./Header";
 import Paragraph from "./Paragraph";
 import Footer from "./Footer";
+import Form from "./Form";
 
 function App() {
   return (
@@ -8,36 +9,14 @@ function App() {
       <Header
         title="Currency converter"
       />
-        <Paragraph
-          text="Convert euros and dollars to Polish zlotys at the exchange rate on 29/03/2023."
-        />
-        <form className="form">
-          <fieldset className="form__fieldset">
-            <legend className="form__legend">
-              Converter
-            </legend>
-            <p className="form__paragraph">
-              <label>
-                <span className="form__labelText">Choose Euro or Dollars:</span>
-                <select className="form__field currency">
-                  <option value="EUR">EUR</option>
-                  <option value="USD">USD</option>
-                </select>
-              </label>
-            </p>
-            <p className="form__paragraph">
-              <label>
-                <span className="form__labelText">Enter the amount:</span>
-                <input className="form__field amount" type="number" required min="1" step="1" />
-              </label>
-            </p>
-          </fieldset>
-          <button className="form__submit">Convert</button>
-        </form>
-        <Paragraph
-          text="Amount in PLN: "
-          value="N/A"
-        />
+      <Paragraph
+        text="Convert euros and dollars to Polish zlotys at the exchange rate on 29/03/2023."
+      />
+      <Form />
+      <Paragraph
+        text="Amount in PLN: "
+        value="N/A"
+      />
       <Footer
         text="Page by Adrian W."
       />
---
+++ src/Form/index.js
@@ -0,0 +1,29 @@
+import "./style.css";
+
+const Form = () => (
+    <form className="form">
+        <fieldset className="form__fieldset">
+            <legend className="form__legend">
+                Converter
+            </legend>
+            <p className="form__paragraph">
+                <label>
+                    <span className="form__labelText">Choose Euro or Dollars:</span>
+                    <select className="form__field currency">
+                        <option value="EUR">EUR</option>
+                        <option value="USD">USD</option>
+                    </select>
+                </label>
+            </p>
+            <p className="form__paragraph">
+                <label>
+                    <span className="form__labelText">Enter the amount:</span>
+                    <input className="form__field amount" type="number" required min="1" step="1" />
+                </label>
+            </p>
+        </fieldset>
+        <button className="form__submit">Convert</button>
+    </form>
+);
+
+export default Form;
