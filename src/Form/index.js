596441e6ed7ad5bf54d8aa4c646539342377c664
Adrian Wozniak <adrianwozniak20@gmail.com>
2023-11-04 19:17:16 +0100

    added function to prevent sending form

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,29 +1,40 @@
 import "./style.css";
 
-const Form = () => (
-    <form className="form">
-        <fieldset className="form__fieldset">
-            <legend className="form__legend">
-                Converter
-            </legend>
-            <p className="form__paragraph">
-                <label>
-                    <span className="form__labelText">Choose Euro or Dollars:</span>
-                    <select className="form__field">
-                        <option value="EUR">EUR</option>
-                        <option value="USD">USD</option>
-                    </select>
-                </label>
-            </p>
-            <p className="form__paragraph">
-                <label>
-                    <span className="form__labelText">Enter the amount:</span>
-                    <input className="form__field" type="number" required min="1" step="1" />
-                </label>
-            </p>
-        </fieldset>
-        <button className="form__submit">Convert</button>
-    </form>
-);
+const Form = () => {
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <form className="form">
+            <fieldset className="form__fieldset">
+                <legend className="form__legend">
+                    Converter
+                </legend>
+                <p className="form__paragraph">
+                    <label>
+                        <span className="form__labelText">Choose Euro or Dollars:</span>
+                        <select className="form__field">
+                            <option value="EUR">EUR</option>
+                            <option value="USD">USD</option>
+                        </select>
+                    </label>
+                </p>
+                <p className="form__paragraph">
+                    <label>
+                        <span className="form__labelText">Enter the amount:</span>
+                        <input className="form__field" type="number" required min="1" step="1" />
+                    </label>
+                </p>
+            </fieldset>
+            <button
+                className="form__submit"
+                onClick={onFormSubmit}
+            >
+                Convert
+            </button>
+        </form>
+    )
+};
 
 export default Form;
