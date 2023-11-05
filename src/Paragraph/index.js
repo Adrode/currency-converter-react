283535cccd8d1f802acd6819ddd136239d8f5aeb
Adrian Wozniak <adrianwozniak20@gmail.com>
2023-11-05 18:45:08 +0100

    added <strong> tag for result value

--- src/Paragraph/index.js
+++ src/Paragraph/index.js
@@ -1,7 +1,7 @@
 import "./style.css";
 
 const Paragraph = ({ text, value }) => (
-    <p className="paragraph">{text}{value}</p> // tutaj chciałem zmienić value na value.toFixed(2), lecz to również nie działa i jest błąd całej aplikacji
+    <p className="paragraph">{text}<strong>{value}</strong></p> // tutaj chciałem zmienić value na value.toFixed(2), lecz to również nie działa i jest błąd całej aplikacji
 );
 
 export default Paragraph;
