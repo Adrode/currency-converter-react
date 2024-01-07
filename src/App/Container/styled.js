0a519c70828d0626c688600f79bc064450c3df3d
Adrian Wozniak <adrianwozniak20@gmail.com>
2024-01-07 17:32:28 +0100

    box-shadowapplied to Container

--- src/App/Container/styled.js
+++ src/App/Container/styled.js
@@ -3,6 +3,6 @@ import styled from "styled-components";
 export const StyledContainer = styled.main`
     padding: 50px;
     background-color: ${({ theme }) => theme.color.tundora};
-    border: 1px solid ${({ theme }) => theme.color.black};
     border-radius: 20px;
+    box-shadow: 5px 5px 5px ${({ theme }) => theme.color.mineShaft};
 `;
