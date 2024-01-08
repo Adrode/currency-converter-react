f2acb195d46338abdf6b8ebc515f2d366a0f9235
Adrian Wozniak <adrianwozniak20@gmail.com>
2024-01-08 19:30:27 +0100

    moved newCurrencies.json to public catalog

--- src/App/Fetch/index.js
+++ src/App/Fetch/index.js
@@ -1,12 +1,17 @@
 import { useState, useEffect } from "react";
 
 const Fetch = () => {
-    const [data, setData] = useState({});
+    const [ratesData, setRatesData] = useState({});
 
     useEffect(() => {
-        fetch("/https://api.currencyapi.com/v3/latest?apikey=cur_live_l6j0fBqqMwtFBjnvWA9uk2Tg0I8v8lrO8q3FBHEi")
+        fetch("public/newCurrencies.json")
             .then(response => response.json())
             .then(currencies => console.log(currencies));
+        /*fetch("/https://api.currencyapi.com/v3/latest?apikey=cur_live_l6j0fBqqMwtFBjnvWA9uk2Tg0I8v8lrO8q3FBHEi")
+            .then(response => response.json())
+            .then(currencies => {
+                setRatesData((ratesData) => ratesData = { ...currencies })
+            });*/
     }, []);
 };
 
